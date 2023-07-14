727759023ed1be73680a42eaeea55952b0a8ac7a
Daizhiyuan <daizhiyuan@phytium.com.cn>
2023-07-14 16:56:47 +0800

    kernel: Implementation get current execption level function
    Signed-off-by: Daizhiyuan <daizhiyuan@phytium.com.cn>

--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include "pl011_uart.h"
 #include "testcase.h"
 
 extern void gicInit(void);
 
+int get_currentEL(void)
+{
+	uint64_t current_EL;
+
+#define EL_SHIFT 2
+#define EL_MASK (0x3 << EL_SHIFT)
+
+	__asm volatile ("MRS %[result], CurrentEL"
+			: [result] "=r" (current_EL)
+			:
+			:
+	);
+
+	return (current_EL & EL_MASK) >> EL_SHIFT;
+}
+
 int main(void)
 {
+
     /* Infrastructure initialization segment */
     uartInit();
     gicInit();
+
+    printf("Current Exception Level: [%d]\n\n", get_currentEL());
 
     /* The actual testcase segment */
     dump_smmu_idr0();
